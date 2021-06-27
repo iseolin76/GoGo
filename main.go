328f6cbd30db0249c0cd1f4360e40a429331e8ab
Seolin <65021545+iseolin76@users.noreply.github.com>
2021-06-27 17:01:49 +0900

    Revert "Feature/help embed"

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/iseolin76/GoGo/message"
+	"github.com/iseolin76/GoGo/config"
 )
 
 var (
@@ -21,7 +22,7 @@ func main() {
 		return
 	}
 
-	dg.AddHandler(message.MessageCreate)
+	dg.AddHandler(messageCreate)
 
 	dg.Identify.Intents = discordgo.IntentsGuildMessages
 
@@ -37,4 +38,32 @@ func main() {
 	<-sc
 
 	dg.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	//봇이 보낸 메시지인지 판별
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	//명령어 검사
+	msg := strings.Split(m.Content, " ")
+	if msg[0] == config.ADD_COMMAND || msg[0] == config.DELETE_COMMAND {
+		s.ChannelMessageSend(m.ChannelID, "준비 중인 기능입니다.")
+	}
+	if msg[0] != config.PREFIX  {
+		return
+	}
+
+	//응답
+	fmt.Println(msg)
+	if msg[1] == "초대코드" {
+		s.ChannelMessageSend(m.ChannelID, config.INVITE_URL)
+	}
+	if msg[1] == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+	if msg[1] == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
 }
