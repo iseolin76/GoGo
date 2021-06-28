396b4c31ec8c2c219877a20eaf39ea5ec2520ce2
iseolin76 <iseolin76@gmail.com>
2021-06-28 09:55:34 +0900

    add: histimetable api connection

--- api/formatapi_neis.go
+++ api/formatapi_neis.go
@@ -41,3 +41,34 @@ func NeisMealServiceDietInfo(date string) *discordgo.MessageEmbed {
 
 	return embed.EmbedNeisMealServiceDietInfo(schul, ymd, menus)
 }
+
+// 필요인자: 가져올 날짜 ex) 20201203
+
+// 나이스에서 가져온 시간표 정보를 임베드로 만들어 리턴합니다.
+func NeisHisTimetable(date, grade, classNm string) *discordgo.MessageEmbed {
+	//json을 HisTimetableType Unmarshaling
+	result := requestApi(hisTimetable(date, grade, classNm))
+	var data types.HisTimetableType
+	json.Unmarshal(result, &data)
+
+
+	//데이터가 비었을 경우 데이터가 없다는 임베드를 리턴합니다.
+	if data.HisTimetable == nil {
+		return embed.NoDataEmbed()
+	}
+
+	timeTable := data.HisTimetable[1].Row
+
+	var times = make(map[string]string)
+
+	for _, time := range timeTable {
+		times[time.Perio] = time.ItrtCntnt
+	}
+
+	schul := timeTable[0].SchulNm
+	ymd:= timeTable[0].AllTiYmd
+	grade = timeTable[0].Grade
+	classNm = timeTable[0].ClassNm
+
+	return embed.EmbedNeisHisTimetable(schul, ymd, grade, classNm, times)
+}
--- embed/apiembed.go
+++ embed/apiembed.go
@@ -32,3 +32,26 @@ func EmbedNeisMealServiceDietInfo(schul, ymd string, menus []string) *discordgo.
 	return mealEmbed.MessageEmbed;
 }
 
+
+// 필요인자: schul: 학교 이름, ymd: 시간표 일자, grade: 학년, classNm: 반, menus: 시간표
+
+// 학교 시간표 정보로 임베드를 만듭니다.
+func EmbedNeisHisTimetable(schul, ymd, grade, classNm string, times map[string]string) *discordgo.MessageEmbed {
+	timeTableEmbed := embed.NewEmbed().
+	SetTitle(ymd[:4]+"년 "+ymd[4:6]+"월 "+ymd[6:]+"일").
+	SetDescription(schul+"\n"+grade+"학년 "+classNm+"반 시간표")
+
+	//길이에 따라 다르게 임베드를 생성합니다.
+	for idx := range times {
+		timeTableEmbed = timeTableEmbed.
+		AddField(string(idx)+"교시", times[string(idx)])
+	}
+
+	timeTableEmbed = 
+	timeTableEmbed.
+	Truncate().
+	SetFooter(schul).
+	SetColor(config.GO_COLOR)
+
+	return timeTableEmbed.MessageEmbed;
+}
