f57ff4c4489616feb1de763fb3a80408bc1f644c
iseolin76 <iseolin76@gmail.com>
2021-06-28 14:28:38 +0900

    udpate: 명령어 변경

--- config/config.go
+++ config/config.go
@@ -1,9 +1,10 @@
 package config
 
 const (
-	ADD_COMMAND       string = "고커추"
+	ADD_COMMAND       string = "배워"
 	PREFIX            string = "고"
-	DELETE_COMMAND    string = "고커삭"
+	DELETE_COMMAND    string = "잊어"
+	PREFIX1           string = "ㄱ"
 	INVITE_COMMAND    string = "초대코드"
 	MEAL_COMMAND      string = "밥"
 	INVITE_URL        string = "https://discord.com/api/oauth2/authorize?client_id=700963738966163487&permissions=8&scope=bot"
