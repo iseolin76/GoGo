158f72f4ab35b45a09d6c93cbd47a9d5709f7c37
iseolin76 <iseolin76@gmail.com>
2021-06-28 14:30:30 +0900

    update: ㄱ명령어 지원

--- message/message.go
+++ message/message.go
@@ -21,40 +21,46 @@ func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	//명령어 검사
 	msg := strings.Split(m.Content, " ")
 
+	if(len(msg[0]) > 1) {
+		msg1 := strings.Split(m.Content, "");
+		if msg1[0] != config.PREFIX && msg1[0] != config.PREFIX1 {
+			return
+		} else {
+			msg = strings.Split(strings.Join(msg1[1:], ""), " ")
+		}
+	}
+
 	if msg[0] == config.ADD_COMMAND || msg[0] == config.DELETE_COMMAND {
 		s.ChannelMessageSend(m.ChannelID, "준비 중인 기능입니다.")
 	}
 
-	if msg[0] != config.PREFIX || len(msg) < 2 {
-		return
-	}
-
 	//고 응답
-	if msg[1] == config.INVITE_COMMAND {
+	if msg[0] == config.INVITE_COMMAND {
 		s.ChannelMessageSend(m.ChannelID, config.INVITE_URL)
 	}
 
 	//밥 명령어
-	if msg[1] == config.MEAL_COMMAND || 
-	(len(msg) > 2 && msg[2] == config.MEAL_COMMAND) || 
-	(len(msg) > 3 && msg[3] == config.MEAL_COMMAND) {
+	if msg[0] == config.MEAL_COMMAND ||
+	msg[0] == "ㅂ" ||
+	(len(msg) > 1 && msg[1] == config.MEAL_COMMAND) || 
+	(len(msg) > 2 && msg[2] == config.MEAL_COMMAND) {
 		now := time.Now();
 		var date string;
 
-		if len(msg) > 2 {
+		if len(msg) > 1 {
 			var message string
 
 			//명령어 판별
-			if msg[1] == config.MEAL_COMMAND {
-				if len(msg) == 4 {
-					message = msg[2] + " " + msg[3]
+			if msg[0] == config.MEAL_COMMAND || msg[0] == "ㅂ" {
+				if len(msg) == 3 {
+					message = msg[1] + " " + msg[2]
 				} else {
-				message = msg[2]
-				}
-			} else if msg[2] == config.MEAL_COMMAND {
 				message = msg[1]
-			} else if len(msg) > 3 && msg[3] == config.MEAL_COMMAND {
-				message = msg[1] + " " + msg[2]
+				}
+			} else if msg[1] == config.MEAL_COMMAND {
+				message = msg[0]
+			} else if len(msg) > 2 && msg[2] == config.MEAL_COMMAND {
+				message = msg[0] + " " + msg[1]
 			}
 			now = util.CheckDate(message, now)
 		}
@@ -64,31 +70,31 @@ func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	//도움말 명령어
-	if msg[1] == config.HELP_COMMAND {
+	if msg[0] == config.HELP_COMMAND {
 		s.ChannelMessageSendEmbed(m.ChannelID, embed.HelpEmbed())
 	}
 
 	//시간표 명령어
-	if msg[1] == config.TIMETABLE_COMMAND || 
-	(len(msg) > 2 && msg[2] == config.TIMETABLE_COMMAND) ||
-	(len(msg) > 3 && msg[3] == config.TIMETABLE_COMMAND) {
-		if msg[len(msg)-1] != config.TIMETABLE_COMMAND && len(msg) > 3 {
+	if msg[0] == config.TIMETABLE_COMMAND || 
+	(len(msg) > 1 && msg[1] == config.TIMETABLE_COMMAND) ||
+	(len(msg) > 2 && msg[2] == config.TIMETABLE_COMMAND) {
+		if msg[len(msg)-1] != config.TIMETABLE_COMMAND && len(msg) > 2 {
 			now := time.Now();
 			var date string;
 			var message string
 			var grade, classNm string
 
 			//명령어 판별
-			if len(msg) > 5 {
-				if len(msg) == 5 && msg[2] == config.TIMETABLE_COMMAND {
-					message = msg[1]
+			if len(msg) > 3 {
+				if len(msg) == 4 && msg[1] == config.TIMETABLE_COMMAND {
+					message = msg[0]
+					grade = msg[2]
+					classNm = msg[3]
+					now = util.CheckDate(message, now)
+				} else if len(msg) == 5 && msg[2] == config.TIMETABLE_COMMAND {
+					message = msg[0]+" "+msg[1]
 					grade = msg[3]
 					classNm = msg[4]
-					now = util.CheckDate(message, now)
-				} else if len(msg) == 6 && msg[3] == config.TIMETABLE_COMMAND {
-					message = msg[1]+" "+msg[2]
-					grade = msg[4]
-					classNm = msg[5]
 					now = util.CheckDate(message, now)
 				}
 			}
@@ -96,7 +102,7 @@ func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 			date = now.Format("20060102")
 			s.ChannelMessageSendEmbed(m.ChannelID, api.NeisHisTimetable(date, grade, classNm))
 		} else {
-			s.ChannelMessageSend(m.ChannelID, `형식은 *"고 [일자] 시간표 [학년] [반]"* 이에요!`+"\nex) 고 내일 시간표 2 1, 고 시간표 1 1...")
+			s.ChannelMessageSend(m.ChannelID, `형식은 *"고[일자] 시간표 [학년] [반]"* 이에요!`+"\nex) 고내일 시간표 2 1, 고시간표 1 1...")
 		}
 	}
 }
