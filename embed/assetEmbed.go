0ab221090f6718b2543d2ba867bc6161678dce04
iseolin76 <iseolin76@gmail.com>
2021-06-28 14:30:44 +0900

    updaet: 띄어쓰기 삭제

--- embed/assetEmbed.go
+++ embed/assetEmbed.go
@@ -10,13 +10,13 @@ import (
 func HelpEmbed() *discordgo.MessageEmbed {
 	helpEmbed := embed.NewEmbed().
 	SetTitle("GoGo 도움말").
-	SetDescription("'고'를 부르면 나타난다! GoGo.\n개발자 : ㅎㄹㅁㄹ#5146").
+	SetDescription("'고'를 부르면 나타난다! GoGo.\n접두사 : 'ㄱ'또는 '고'\n개발자 : ㅎㄹㅁㄹ#5146").
 	AddField(config.INVITE_COMMAND, "GoGo를 초대할 수 있는 링크가 나타납니다.").
-	AddField(config.MEAL_COMMAND, "거의 핵심인가?\n**광소마고의 급식**을 보여줍니다.").
+	AddField(config.MEAL_COMMAND, "거의 핵심인가?\n광소마고의 급식을 보여줍니다.\n초성도 돼요. ㄱㅂ, ㄱㅂ 내일, ㄱ밥 ...").
 	AddField(config.ADD_COMMAND, "사용자 커맨드를 추가할 수 있어요!\n사실 귀찮아 아직 안 만듬").
 	AddField(config.DELETE_COMMAND, "사용자 커맨드를 삭제할 수 있어요!\n사실 귀찮아 아직 안 만듬").
 	AddField(config.HELP_COMMAND, "GoGo의 명령어 목록과 설명을 보여줘요.\n너가 보고 있는 이거").
-	AddField(config.TIMETABLE_COMMAND, "광소마고의 시간표를 보여줘요.\n형식 고 [일자(생략가능)] 시간표 [학년] [반]\nex) 고 다음주 월요일 시간표 2 1").
+	AddField(config.TIMETABLE_COMMAND, "광소마고의 시간표를 보여줘요.\n형식 고[일자(생략가능)] 시간표 [학년] [반]\nex) 고다음주 월요일 시간표 2 1").
 	SetFooter("농협) 356-1052-3698-23 예금주 이서린 \n<- 후원하면 진짜 열심히 일해볼게요..ㅎㅎ \n후원이라고 적고 보내줘야 함").
 	SetColor(config.GO_COLOR).MessageEmbed
 
